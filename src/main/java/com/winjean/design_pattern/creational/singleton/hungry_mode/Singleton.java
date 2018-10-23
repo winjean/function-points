d45639239a65444fef515139a2287caa6a97dc0b
wangjian <wangjian@cnaidun.com>
2018-10-23 13:48:17 +0800

    add singleton

---
+++ src/main/java/com/winjean/design_pattern/creational/singleton/hungry_mode/Singleton.java
@@ -0,0 +1,23 @@
+package com.winjean.designPattern.creational.singleton.hungry_mode;
+
+/**
+ * 项目名称：重庆微警务（一期）
+ * 类名称：<....>
+ * 类描述：<....>
+ * 创建人：Administrator
+ * 创建时间：2018/10/23 13:39
+ * 修改人：Administrator
+ * 修改时间：2018/10/23 13:39
+ * 修改备注：
+ * 版权所有权：江苏艾盾网络科技有限公司
+ *
+ * @version V1.0
+ */
+public class Singleton {
+    private static Singleton instance = new Singleton();
+
+    public static Singleton getUniqueInstance() {
+        return instance;
+    }
+
+}
---
+++ src/main/java/com/winjean/design_pattern/creational/singleton/lazy_mode/Singleton.java
@@ -0,0 +1,32 @@
+package com.winjean.designPattern.creational.singleton.lazy_mode;
+
+/**
+ * 项目名称：重庆微警务（一期）
+ * 类名称：<....>
+ * 类描述：<....>
+ * 创建人：Administrator
+ * 创建时间：2018/10/23 11:57
+ * 修改人：Administrator
+ * 修改时间：2018/10/23 11:57
+ * 修改备注：
+ * 版权所有权：江苏艾盾网络科技有限公司
+ *
+ * @version V1.0
+ */
+public class Singleton {
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getUniqueInstance() {
+        if (uniqueInstance == null) {
+            synchronized (Singleton.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
