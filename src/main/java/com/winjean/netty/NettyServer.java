445823b2a270f6ef97447163ef2eaf908353cf16
wangjian <wangjian@cnaidun.com>
2018-11-30 16:44:45 +0800

    update

--- src/main/java/com/winjean/netty/NettyServer.java
+++ src/main/java/com/winjean/netty/NettyServer.java
@@ -4,7 +4,10 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 /**
  * 项目名称：重庆微警务（一期）
@@ -60,7 +63,18 @@ public class NettyServer {
         @Override
         protected void initChannel(Channel channel) throws Exception {
 //            channel.pipeline().addLast(new TimeServerHandler());
-            channel.pipeline().addLast(new HttpServerCodec());
+            //将字节解码为HttpRequest、HttpContent 和LastHttpContent。并将HttpRequest、HttpContent 和LastHttpContent 编码为字节
+//            channel.pipeline().addLast(new HttpServerCodec());
+            //HTTP请求消息解码器
+            channel.pipeline().addLast("http-request-decoder", new HttpRequestDecoder());
+            //添加HttpObjectAggregator解密器，其作用是将多个消息转换为单一的FullHttpRequest或者FullHttpResponse，
+            //原因是HTTP解码器在每个HTTP消息中会生成多个消息对象：有1、HttpRequest/HttpResponse;2、HttpContent；3、LastHttpContent；
+            channel.pipeline().addLast("http-object-aggregator", new HttpObjectAggregator(65536));
+            //HTTP响应编码器
+            channel.pipeline().addLast("http-response-encoder", new HttpResponseEncoder());
+            //支持异步发送大的码流（例如大的文件传输），但不占用过多的内存，防止发生JAVA内存溢出错误
+            channel.pipeline().addLast("chunked-write-handler", new ChunkedWriteHandler());
+
             channel.pipeline().addLast(new HttpServerHandler());
         }
     }
